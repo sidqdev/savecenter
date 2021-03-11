26bf9689154f40416570092faeebf1939308d691
sidq-corp <dimax258223@gmail.com>
2021-03-11 23:38:12 +0200

    Commit

--- loader.py
+++ loader.py
@@ -7,7 +7,7 @@ storage = MongoStorage(host='localhost', port=27017, db_name='aiogram_fsm')
 
 
 API = '1365927493:AAGwlRZpEpWyDTFl4jF0l6fhoxrYMpMaxyU'
-API = '1519913388:AAFReunM6q-zyNjVll6YizuxTyrwvnPsl8s'
+# API = '1519913388:AAFReunM6q-zyNjVll6YizuxTyrwvnPsl8s'
 bot = Bot(token=API, parse_mode='HTML')
 dp = Dispatcher(bot, storage=storage)
 dp.middleware.setup(LoggingMiddleware())
