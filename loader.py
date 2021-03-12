d0a47497093c14c2eed37de57018f7547d335b86
sidq-corp <dimax258223@gmail.com>
2021-03-12 21:47:23 +0200

    env

--- loader.py
+++ loader.py
@@ -1,13 +1,13 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.contrib.fsm_storage.mongo import MongoStorage
-
+import os
 
 storage = MongoStorage(host='localhost', port=27017, db_name='aiogram_fsm')
 
+#
 
-API = '1365927493:AAGwlRZpEpWyDTFl4jF0l6fhoxrYMpMaxyU'
-# API = '1519913388:AAFReunM6q-zyNjVll6YizuxTyrwvnPsl8s'
+API = str(os.getenv('scapi'))
 bot = Bot(token=API, parse_mode='HTML')
 dp = Dispatcher(bot, storage=storage)
 dp.middleware.setup(LoggingMiddleware())
